5cb0accd9bdb96255865f5689e8edbf003a1793a
Viktor Vallmark <vallmark.viktor@gmail.com>
2025-09-15 13:05:07 +0200

    second commit
    Signed-off-by: Viktor Vallmark <vallmark.viktor@gmail.com>

--- src/orthogonalization.jl
+++ src/orthogonalization.jl
@@ -7,13 +7,22 @@ export gramschmidt
 
 function gramschmidt!(matrix::Matrix{UInt8})
 
+    temp = matrix
     
-    
-    for i in size(matrix,1)
+    #first step in the modified Gram-Schmidt, replacing all columns with first column
+    matrix .= temp[:, 1]
 
+    for i in eachindex(matrix)
+        r_ii = norm(matrix[:,i])
+        if r_ii ≈ 0
+            q_i = zeros(size(matrix, 1))
+        end
+        q_i = matrix[:, 1] / r_ii
+        j = i + 1
+        for j in eachindex(matrix)
+            
+        end
     end
-
-
     
 
 
